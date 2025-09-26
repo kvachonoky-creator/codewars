19b94c57e6153553d0a570eb02387adcf7353620
ya_yarek <kvachonoky@gmail.com>
2025-09-26 23:21:42 +0300

    my commit

---
+++ 8kyu/Do_u_speak_english.js
@@ -0,0 +1,17 @@
+// function spEng(sentence) {
+//     if (sentence.includes("English")) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// //write your code here
+// }
+
+function spEng(sentence) {
+    sentence = sentence.toLowerCase();
+    return sentence.includes("english") ? true : false
+//write your code here
+}
+
+
+console.log(spEng("greogheEnGlishd"))
