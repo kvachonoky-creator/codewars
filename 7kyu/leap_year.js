74eb9732ff06ade91ce9420b40d83987c63fc054
ya_yarek <kvachonoky@gmail.com>
2025-10-06 22:03:55 +0300

    my commit

---
+++ 7kyu/leap_year.js
@@ -0,0 +1,13 @@
+const years = 2004;
+
+function isLeapYear(year) {
+  if (year % 100 === 0 && year % 400 === 0) {
+    return true
+  }
+  else if (year % 4 === 0 && year % 100 !== 0) {
+   return true;
+  } else {
+    return false
+  }
+  // TODO
+}
