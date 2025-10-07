367aa2ce5c0909faa13d969e1eb29d6826847946
ya_yarek <kvachonoky@gmail.com>
2025-10-07 21:36:14 +0300

    my commit

---
+++ 8kyu/Switch_it_Up!.js
@@ -0,0 +1,37 @@
+function switchItUp(number){
+    switch (number) {
+        case 0:
+            number = "Zero"
+            break;
+        case 1:
+            number = "One"
+            break;
+        case 2:
+            number = "Two"
+            break;
+        case 3:
+            number = "Three"
+            break;
+        case 4:
+            number = "Four"
+            break;
+        case 5:
+            number = "Five"
+            break;
+        case 6:
+            number = "Six"
+            break;
+        case 7:
+            number = "Seven"
+            break;
+        case 8:
+            number = "Eight"
+            break;
+        case 9:
+            number = "Nine"
+            break;
+    } return number
+//Write your own Code!
+}
+
+console.log(switchItUp(4));
