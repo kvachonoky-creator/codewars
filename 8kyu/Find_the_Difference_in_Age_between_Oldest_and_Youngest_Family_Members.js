fb7df04eed7b04b92d704e7fb759c577e30ab88d
ya_yarek <kvachonoky@gmail.com>
2025-10-09 21:47:00 +0300

    my commit

---
+++ 8kyu/Find_the_Difference_in_Age_between_Oldest_and_Youngest_Family_Members.js
@@ -0,0 +1,22 @@
+// const array = [20, 45, 85, 42, 67, 24, 90, 5, 11, 23]
+
+// function differenceInAges(ages) {
+//     newArr = []
+//     let smallestAge = 150;
+//     let olderAge = 0;
+//     for (let i = 0; i < ages.length; i++) {
+//         if (ages[i] < smallestAge) {
+//             smallestAge = ages[i]
+//         }
+//     }
+//     for (let i = 0; i < ages.length; i++) {
+//         if (ages[i] > olderAge) {
+//             olderAge = ages[i]
+//         }
+//     }
+//     const diffrentAge = olderAge - smallestAge
+//     newArr.push(smallestAge, olderAge, diffrentAge)
+//     return newArr;
+// }
+
+// console.log(differenceInAges(array));
