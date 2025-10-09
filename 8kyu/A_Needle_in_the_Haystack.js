17f489f8c6b8ff85b034641ce5c42f48f9ae611b
ya_yarek <kvachonoky@gmail.com>
2025-10-09 21:56:26 +0300

    my commit

---
+++ 8kyu/A_Needle_in_the_Haystack.js
@@ -0,0 +1,12 @@
+// const arr = ['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]
+
+// function findNeedle(haystack) {
+//   for (let i = 0; i < haystack.length; i++) {
+//     if (haystack[i] === "needle") {
+//       return `found the needle at position ${i}`
+//     }
+//   }
+//   // your code here
+// }
+
+// console.log(findNeedle(arr));
