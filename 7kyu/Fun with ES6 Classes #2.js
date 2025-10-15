b5507b0d1681a944f2141eb8e26f87b776d974ee
ya_yarek <kvachonoky@gmail.com>
2025-10-15 21:52:46 +0300

    my commit

---
+++ 7kyu/Fun with ES6 Classes #2.js
@@ -0,0 +1,46 @@
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super()
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.legs = 0;
+    this.species = "shark";
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`
+  }
+
+}
+
+class Cat extends Animal {
+   constructor(name, age, status) {
+     super()
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.legs = 4;
+    this.species = "cat";
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super()
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.legs = 4;
+    this.species = "dog";
+    this.master = master;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`
+  }
+  greetMaster () {
+    return `Hello ${this.master}`
+  }
+}
