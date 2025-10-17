48e0366bd9fe1caa28340c23e7cbf2ac9c1131c2
ya_yarek <kvachonoky@gmail.com>
2025-10-17 22:09:15 +0300

    my commit

--- 7kyu/friend or foe.js
+++ 7kyu/friend or foe.js
@@ -1,10 +1,14 @@
 
-function friend(friends) {
-    const newFriends = []
-    for (let i = 0; i < friends.length; i++) {
-        if (friends[i].length === 4) {
-            newFriends.push(friends[i])
-        }
-    }
-    return newFriends
+// function friend(friends) {
+//     const newFriends = []
+//     for (let i = 0; i < friends.length; i++) {
+//         if (friends[i].length === 4) {
+//             newFriends.push(friends[i])
+//         }
+//     }
+//     return newFriends
+// }
+
+function friend(friends){
+  return friends.filter(f => f.length === 4)
 }
