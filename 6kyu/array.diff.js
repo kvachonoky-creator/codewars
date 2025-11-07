2b5ab4778f7365c3419210df95a8cc51583c1d39
ya_yarek <kvachonoky@gmail.com>
2025-11-07 23:05:53 +0300

    my commit

--- 6kyu/array.diff.js
+++ 6kyu/array.diff.js
@@ -1,14 +1,9 @@
-const test1 = [1, 2, 3]
-const test2 = [1, 2]
 function arrayDiff(a, b) {
-  const newArr = []
-  for (let i = 0; i < b.length; i++) {
-    for (let j = 0; j < a.length; j++) {
-      if (b[i] !== a[j] && !newArr.includes(b[i])) {
-        newArr.push(a[j])
-      }
+  newArr = []
+  a.forEach(elem => {
+    if (!b.includes(elem)) {
+      newArr.push(elem)
     }
-  } return newArr
+  })
+  return newArr;
 }
-
-console.log(arrayDiff(test1, test2));
